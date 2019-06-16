05528e25813d339f171fa367f4b078fa11e1812c
RafaelZapata <faelzapata2011@gmail.com>
2019-06-16 16:47:59 -0300

    Resolução

--- Curso JS/Introdução 3/todo.js
+++ Curso JS/Introdução 3/todo.js
@@ -2,11 +2,7 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var todos = [
-    'Fazer café',
-    'Estudar Javascript',
-    'Acessar comunidade Rockseat'
-];
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [] //caso não encontre um valor no local storage, ele retorna uma string vazia
 
 function renderTodos() {
     listElement.innerHTML = ''; /*coloca tudo que está dentro da ul vazio para que quando o botão adicionar seja pressionado
@@ -39,6 +35,7 @@ function addTodo() {
     todos.push(todoText); //adiciona elemento ao final do array de todos
     inputElement.value = "";
     renderTodos();
+    saveToStorage(); 
 }
 
 buttonElement.onclick = addTodo;
@@ -46,4 +43,10 @@ buttonElement.onclick = addTodo;
 function deleteTodo(position){
     todos.splice(position, 1);
     renderTodos();
+    saveToStorage();
+}
+
+function saveToStorage() {
+    localStorage.setItem('list_todos', JSON.stringify(todos)); //O json converte o array em uma string e salva no local storage
+
 }
