f89d63ccb19d4e2657befe5347196aee90da3877
RafaelZapata <faelzapata2011@gmail.com>
2019-07-14 14:56:18 -0300

    Adição do Ex5

---
+++ Curso JS/Praticando/Ex5/script.js
@@ -0,0 +1,21 @@
+var btnElement = document.querySelector('#btnCalcular');
+
+btnElement.onclick = function tabuada() {
+    var inputElement = document.querySelector('#number');
+    var table = document.querySelector('#table');
+
+    if(inputElement.value.lenght == 0){
+        alert('Por favor, digite um número');
+    } else {
+        var number = Number(inputElement.value);
+        table.innerHTML = '';
+        for(let i=1; i<=10; i++){
+            let item = document.createElement('label');
+            item.innerHTML = `${number} x ${i} = ${number * i} <br>`
+            table.appendChild(item);
+            // let item = document.createElement('option');
+            // item.text = `${number} x ${i} = ${number * i}`
+            // table.appendChild(item);
+        }
+    }
+}
